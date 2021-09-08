d21c0b75e9ef9be1278b984eb93fa645ac92b148
Pavel Kulyov <kulyov.pavel@gmail.com>
2021-09-08 20:12:04 +0300

    elisp: indent plists more sensibly (thanks @aorst)

--- init.el
+++ init.el
@@ -837,7 +837,62 @@
 
 (use-package elisp-mode
   :ensure nil
-  :delight "elisp")
+  :delight "elisp"
+  :commands aorst/emacs-lisp-indent-function
+  :hook (emacs-lisp-mode . aorst/emacs-lisp-setup)
+  :bind (:map emacs-lisp-mode-map
+         ("C-c C-M-f" . aorst/indent-buffer))
+  :config
+  (defun aorst/emacs-lisp-indent-function (indent-point state)
+    "A replacement for `lisp-indent-function'.
+Indents plists more sensibly. Adapted from DOOM Emacs:
+https://github.com/hlissner/doom-emacs/blob/b03fdabe4fa8a07a7bd74cd02d9413339a485253/modules/lang/emacs-lisp/autoload.el#L91"
+    (let ((normal-indent (current-column))
+          (orig-point (point))
+          target)
+      (goto-char (1+ (elt state 1)))
+      (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+      (cond ((and (elt state 2)
+                  (or (not (looking-at-p "\\sw\\|\\s_"))
+                      (eq (char-after) ?:)))
+             (unless (> (save-excursion (forward-line 1) (point))
+                        calculate-lisp-indent-last-sexp)
+               (goto-char calculate-lisp-indent-last-sexp)
+               (beginning-of-line)
+               (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t))
+             (backward-prefix-chars)
+             (current-column))
+            ((and (save-excursion
+                    (goto-char indent-point)
+                    (skip-syntax-forward " ")
+                    (not (eq (char-after) ?:)))
+                  (save-excursion
+                    (goto-char orig-point)
+                    (and (eq (char-after) ?:)
+                         (eq (char-before) ?\()
+                         (setq target (current-column)))))
+             (save-excursion
+               (move-to-column target t)
+               target))
+            ((let* ((function (buffer-substring (point) (progn (forward-sexp 1) (point))))
+                    (method (or (function-get (intern-soft function) 'lisp-indent-function)
+                                (get (intern-soft function) 'lisp-indent-hook))))
+               (cond ((or (eq method 'defun)
+                          (and (null method)
+                               (> (length function) 3)
+                               (string-match-p "\\`def" function)))
+                      (lisp-indent-defform state indent-point))
+                     ((integerp method)
+                      (lisp-indent-specform method state indent-point normal-indent))
+                     (method
+                      (funcall method indent-point state))))))))
+  (defun aorst/emacs-lisp-setup ()
+    (setq-local lisp-indent-function
+                #'aorst/emacs-lisp-indent-function))
+  (defun org-babel-edit-prep:emacs-lisp (_info)
+    "Setup Emacs Lisp buffer for Org Babel."
+    (setq lexical-binding t)
+    (setq-local flycheck-disabled-checkers '(emacs-lisp-checkdoc))))
 
 (use-package cider
   :ensure t)
